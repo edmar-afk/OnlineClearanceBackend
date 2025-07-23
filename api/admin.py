27d4dc77c5a7cc0e54d3d0754717156fa9ab3262
project2479 <jaywrsnp6@gmail.com>
2025-07-23 17:32:17 +0800

    new

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Programs, Clearance, Signature, Student
+from .models import Programs, Clearance, Signature, Student, ClearanceSignature
 
 admin.site.register(Programs)
 admin.site.register(Clearance)
 admin.site.register(Signature)
 admin.site.register(Student)
+admin.site.register(ClearanceSignature)
